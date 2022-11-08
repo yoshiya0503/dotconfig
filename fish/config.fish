8482fd5fe213f282bac406fb689c54de20a21dba
yoshiya0503 <myon53@gmail.com>
2022-11-08 14:29:30 +0900

    remove gopath goroot

--- fish/config.fish
+++ fish/config.fish
@@ -2,17 +2,16 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   eval (/opt/homebrew/bin/brew shellenv)
 
-  set -x GOPATH $HOME/.go
   set -x PYENV_ROOT $HOME/.pyenv
   set -x N_PREFIX $HOME/.n
   set -x RUSTUP_HOME $HOME/.rustup
   set -x CARGO_HOME $HOME/.cargo
   set -x PATH $PATH $HOME/.n/bin
   set -x PATH $PYENV_ROOT/bin $PATH
-  set -x PATH $PATH $GOPATH/bin
   set -x PATH $PATH $CARGO_HOME/bin
 
   alias vim=nvim
+  alias vi=nvim
   alias kube=kubectl
   alias tilt=/opt/homebrew/bin/tilt
   alias tlt=/opt/homebrew/bin/tilt
@@ -21,7 +20,6 @@ if status is-interactive
   eval rbenv init - | source
 
   set -x PATH $HOME/.poetry/bin $PATH
-  set -x HRB_TILT 1
   set fish_greeting
 
   starship init fish | source
--- nvim/init.lua
+++ nvim/init.lua
@@ -96,7 +96,7 @@ require("tokyonight").setup({
   transparent = true,
   styles = {
     sidebars = "transparent",
-    floats = "transparent",
+    -- floats = "transparent",
   },
 })
 vim.cmd[[colorscheme tokyonight]]
